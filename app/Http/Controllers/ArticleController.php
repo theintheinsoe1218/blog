cf52dd9ff941c094f5669032cf866c0a7933b789
TheinTheinSoe <theintheinsoe1218@gmail.com>
2020-11-17 15:29:56 +0630

    View

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -7,7 +7,13 @@ use Illuminate\Http\Request;
 class ArticleController extends Controller
 {
     public function index(){
-        return "Controller - Article List";
+        $data=[
+            ["id"=>1,"title"=>"First Article"],
+            ["id"=>2,"title"=>"Second Article"],
+        ];
+        return view('articles.index',[
+            'articles'=>$data
+        ]);
     }
     public function detail($id){
         return "Controller - Article Detail - $id";
